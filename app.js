4eaa04e7addc5bdc4841d33afaa61eba37c5dbd4
Lalith <lalithleftie@gmail.com>
2022-02-23 18:51:33 +0530

    completed 15. Authentication

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 const session = require('express-session')
 const MongoDBStore = require('connect-mongodb-session')(session)
+const csrf = require('csurf')
+const flash = require('connect-flash')
 
 const errorController = require('./controllers/error');
 
@@ -17,6 +19,8 @@ const store = new MongoDBStore({
     collection: 'sessions'
 })
 
+const csrfProtection = csrf()
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -28,17 +32,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, store: store}))
 
+app.use(csrfProtection)
+app.use(flash())
+
 app.use((req, res, next) => {
     if(!req.session.user) {
         next()
     } else {
-        User.findById('6213698464d0d3a1cd16207e')
+        User.findById(req.session.user._id)
         .then(user => {
-        req.user = user
-        next()
+            req.user = user
+            next()
         })
         .catch(err => console.log(err))
     }
+})
+
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn,
+    res.locals.csrfToken = req.csrfToken()
+    next()
 })
 
 app.use('/admin', adminRoutes);
@@ -49,16 +62,6 @@ app.use(errorController.get404);
 
 mongoose.connect(MONGODB_URI)
     .then(result => {
-        User.findOne().then(user => {
-            if(!user) {
-                const user = new User({
-                    name: 'Lalith',
-                    email: 'lalith@mail.com',
-                    cart: []
-                })
-                user.save()
-            }
-            app.listen(3000)
-        })
+        app.listen(3000)
     })
     .catch(err => console.log(err))
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,43 @@
 const User = require('../models/user')
+const bcryptjs = require('bcryptjs')
 
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: req.session.isLoggedIn
+    isAuthenticated: req.session.isLoggedIn,
+    errorMessage: req.flash('error')[0]
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('6213698464d0d3a1cd16207e')
-    .then(user => {
-      req.session.isLoggedIn = true
-      req.session.user = user
-      res.redirect('/')
-    })
-    .catch(err => console.log(err))
+  const email = req.body.email
+  const password = req.body.password
+  User.findOne({email: email})
+  .then(user => {
+    if(!user) {
+      req.flash('error', 'Invalid email or password')
+      return res.redirect('/login')
+    }
+    bcryptjs.compare(password, user.password)
+      .then(doMatch => {
+        if(doMatch) {
+          req.session.isLoggedIn = true
+          req.session.user = user
+          return req.session.save(err => {
+            console.log(err)
+            res.redirect('/')
+          })
+        }
+        req.flash('error', 'Invalid email or password')
+        res.redirect('/login')
+      })
+      .catch(err => {
+        console.log(err)
+        res.redirect('/login')
+      })
+  })
+  .catch(err => console.log(err))
 };
 
 exports.postLogout = (req, res, next) => {
@@ -24,3 +46,39 @@ exports.postLogout = (req, res, next) => {
     res.redirect('/')
   })
 };
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false,
+    errorMessage: req.flash('error')[0]
+  });
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email
+  const password = req.body.password
+  const confirmPassword = req.body.confirmPassword
+  User.findOne({email: email}).then(userData => {
+    if(userData) {
+      req.flash('error', 'Email already exists')
+      return res.redirect('/signup')
+    }
+    return bcryptjs.hash(password, 12)
+  })
+  .then(hashedPassword => {
+    const user = new User({
+      email: email,
+      password: hashedPassword,
+      cart: {items: []}
+    })
+    return user.save()
+  })
+  .then(result => {
+    res.redirect('/login')
+  })
+  .catch(err => console.log(err))
+};
+
+
